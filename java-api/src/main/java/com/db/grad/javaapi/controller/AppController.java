8d9927dc095a507309cb2090fb7e5358ca345831
Anuraag <anuraag.k.shankar@gmail.com>
2022-08-09 21:09:19 +0530

    Added CORS to login

--- java-api/src/main/java/com/db/grad/javaapi/controller/AppController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/AppController.java
@@ -3,6 +3,7 @@ package com.db.grad.javaapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.db.grad.javaapi.model.User;
 import com.db.grad.javaapi.repository.UserRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class AppController {
 	@Autowired
